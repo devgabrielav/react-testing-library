87e5166780fc19c7b23ed5e47fe0351031014d4e
devgabrielav <gabrivicentini@icloud.com>
2023-08-10 17:57:47 -0300

    Requisito 4 feito!

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste o componente <NotFound.tsx />', () => {
+  renderWithRouter(<App />, { route: '/random' });
+  const aspa = '\'';
+  const titulo = screen.getByRole('heading', { name: 'Page requested not found' });
+  const image = screen.getByAltText(
+    `Clefairy pushing buttons randomly with text I have no idea what i${aspa}m doing`,
+  );
+
+  expect(titulo).toBeInTheDocument();
+  expect(image).toBeInTheDocument();
+});
